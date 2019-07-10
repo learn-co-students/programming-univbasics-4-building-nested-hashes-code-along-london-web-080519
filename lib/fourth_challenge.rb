6083f052719467281e23d4aa15fbd6a7725fc65b
Angelina Shin Manley <ashin05@gmail.com>
2019-07-10 14:16:18 +0000

    Done.

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -1,23 +1,30 @@
 def fourth_challenge
-  epic_tragedy = {
-   :montague => {
-      :patriarch => {name: "Lord Montague", age: "53"},
-      :matriarch => {name: "Lady Montague", age: "54"},
-      :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => [
-        #your hashes here!
-      ]
-   }, 
-   :capulet => {
-      :patriarch => {name: "Lord Capulet", age: "50"},
-      :matriarch => {name: "Lady Capulet", age: "51"},
-      :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => [
-        #your hashes here!
-      ]
-   }
-}
+    epic_tragedy = {
+     :montague => {
+        :patriarch => { name: "Lord Montague", age: "53"
+          #your key/value pairs here
+          },
+        :matriarch => { name: "Lady Montague", age: "54"        #your key/value pairs here
+          },
+        :hero => { name: "Romeo", age: "15", status: "alive"
+          #your key/value pairs here
+          },
+        :hero_friends => [{name: "Benvolio", age: "17", attitude: "worried"}, {name: "Mercutio", age: "18", attitude: "hot-headed"}]
+     },
+     :capulet => {
+        :patriarch => { name: "Lord Capulet", age: "50"
+          #your key/value pairs here
+          },
+        :matriarch => { name: "Lady Capulet", age:  "51"
+          #your key/value pairs here
+          },
+        :heroine => { name: "Juliet", age: "15", status: "alive"
+          #your key/value pairs here
+          },
+        :heroine_friends => [{name: "Steven", age: "30", attitude: "confused"}, {name: "Nurse", age: "44", attitude: "worried"}]
 
-  
+     }
+    }
 
-end
+
+  end
